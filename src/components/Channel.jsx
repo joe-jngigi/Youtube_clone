ad604fdae8a604478ed16fc9f0edd259afe1e97c
joe-jngigi <josephngigi775@gmail.com>
2022-10-14 21:11:13 +0300

    Videos rendered successfully

--- src/components/Channel.jsx
+++ src/components/Channel.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-const Channel = () => {
+import { Typography, Box, CardContent, CardMedia } from '@mui/material'
+import { CheckCircle } from '@mui/icons-material'
+
+import { demoProfilePicture } from '../utils/constanst'
+const Channel = ({channel}) => {
   return (
-    <div>Channel</div>
+    <Box className='rounded-2xl'>
+      <Link to = {`/channel/${channel?.id?.channelId}`}>
+        <CardContent className='flex flex-col justify-center text-center text-gray-100'>
+          <CardMedia className='h-48 w-48 rounded-full' image= {channel?.snippet?.thumbnails?.high?.url || demoProfilePicture} alt = {channel?.snippet?.title} />
+          <Typography>
+            {channel?.snippet?.title}
+            <CheckCircle className='font-semibold ml-1 text-gray-400'/>
+          </Typography>
+          {channel?.statistics?.subscriberCount && (
+            <Typography>
+            {parseInt(channel?.statistics?.subscriberCount).toLocaleString()} Subscribers
+            
+            </Typography>
+          )}
+        </CardContent>
+      </Link>
+    </Box>
   )
 }
 
--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,8 +1,34 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-const VideoCard = () => {
+import { Typography, Card, CardContent, CardMedia } from '@mui/material'
+
+import { CheckCircle } from '@mui/icons-material'
+
+import { demoThumbnailUrl, demoVideoUrl, demoVideoTitle, demoChannelUrl, demoChannelTitle } from '../utils/constanst'
+
+const VideoCard = ({video: {id: {videoId},snippet }}) => {
+  console.log(videoId, snippet)
   return (
-    <div>VideoCard</div>
+    <Card className=''>
+      <Link to = {videoId ? `/video/${videoId}` : demoVideoUrl}>
+        <CardMedia image = {snippet?.thumbnails?.high?.url} alt = {snippet?.title} className = 'w-full h-44'/>
+      </Link>
+
+      <CardContent className='bg-main-dark-bg h-24'>
+        <Link to = {videoId ? `/video/${videoId}` : demoVideoUrl}>
+          <Typography variant='subtitle1' className='font-bold text-gray-200'>
+            {snippet?.title.slice(0, 60) || demoVideoTitle.slice(0, 60)}
+          </Typography>
+        </Link>
+        <Link to = {snippet?.channelId ? `/video/${snippet?.channelId}` : demoChannelUrl}>
+          <Typography variant='subtitle2' className='font-bold text-gray-400'>
+            {snippet?.channelTitle || demoChannelTitle}
+            <CheckCircle className='font-semibold ml-1 text-gray-400'/>
+          </Typography>
+        </Link>
+      </CardContent>
+    </Card>
   )
 }
 
